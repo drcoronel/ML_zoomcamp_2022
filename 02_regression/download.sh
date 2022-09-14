679898d8f20ef7ea79abfb4dd4521a075c9c272d
user.email <daricoal@gmail.com>
2022-09-14 16:21:07 +0200

    Add 02_regression class and a dataset download script

---
+++ 02_regression/download.sh
@@ -0,0 +1,8 @@
+# !/bin/sh
+# Download the two datasets we need for this lesso
+
+echo "Download lesson dataset"
+wget https://raw.githubusercontent.com/alexeygrigorev/mlbookcamp-code/master/chapter-02-car-price/data.csv
+
+echo "Download homework dataset"
+wget https://raw.githubusercontent.com/alexeygrigorev/datasets/master/housing.csv
