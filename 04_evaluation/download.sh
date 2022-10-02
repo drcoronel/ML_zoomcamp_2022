44ea7944480cac96b0abb32773ea09c46d057cb5
drcoronel <daricoal@gmail.com>
2022-10-02 18:29:31 +0200

    Finished Lesson's notebook and homework

--- 04_evaluation/download.sh
+++ 04_evaluation/download.sh
@@ -4,4 +4,4 @@
 echo "Download lesson dataset"
 wget https://raw.githubusercontent.com/alexeygrigorev/mlbookcamp-code/master/chapter-03-churn-prediction/WA_Fn-UseC_-Telco-Customer-Churn.csv -O data-week-3.csv
 echo "Download homework dataset"
-#wget wget https://raw.githubusercontent.com/alexeygrigorev/datasets/master/housing.csv 
+wget wget wget https://raw.githubusercontent.com/alexeygrigorev/datasets/master/AER_credit_card_data.csv 
