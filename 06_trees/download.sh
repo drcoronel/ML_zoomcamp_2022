34096790b70ae01177cc701381ad688c364149c8
drcoronel <daricoal@gmail.com>
2022-10-07 12:19:54 +0200

    Start week6 notebook and add download.sh script

---
+++ 06_trees/download.sh
@@ -0,0 +1,8 @@
+# !/bin/sh
+# Download the two datasets we need for this lesso
+
+echo "Download lesson dataset"
+wget https://raw.githubusercontent.com/alexeygrigorev/mlbookcamp-code/master/chapter-06-trees/CreditScoring.csv 
+
+#echo "Download homework dataset"
+#wget wget wget https://raw.githubusercontent.com/alexeygrigorev/datasets/master/AER_credit_card_data.csv 
