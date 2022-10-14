10b37a2c970188b996f05d54687e49fbbe40accf
drcoronel <daricoal@gmail.com>
2022-10-14 11:47:02 +0200

    Add homework notebook, fix typo in Lesson

--- 06_trees/download.sh
+++ 06_trees/download.sh
@@ -4,5 +4,5 @@
 echo "Download lesson dataset"
 wget https://raw.githubusercontent.com/alexeygrigorev/mlbookcamp-code/master/chapter-06-trees/CreditScoring.csv 
 
-#echo "Download homework dataset"
-#wget wget wget https://raw.githubusercontent.com/alexeygrigorev/datasets/master/AER_credit_card_data.csv 
+echo "Download homework dataset"
+wget https://raw.githubusercontent.com/alexeygrigorev/datasets/master/housing.csv
